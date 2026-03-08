d4afb6360a11e8fcee800977f6dadd173d116347
Geardung <Geardung@ya.ru>
2026-03-08 11:39:10 +0300

    добавил сканнер короче

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,12 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import DevPage from './pages/DevPage'
 import AnalyticsPage from './pages/AnalyticsPage'
 
-function RedirectToBot() {
-  window.location.replace('https://t.me/Show_Circus_bot')
-  return null
-}
-
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<RedirectToBot />} />
-      <Route path="/dev" element={<DevPage />} />
+      <Route path="/" element={<Navigate to="/scanner" replace />} />
+      <Route path="/dev" element={<Navigate to="/scanner" replace />} />
+      <Route path="/scanner" element={<DevPage />} />
       <Route path="/analytics" element={<AnalyticsPage />} />
       <Route path="/analytics/*" element={<AnalyticsPage />} />
       <Route path="*" element={<Navigate to="/analytics" replace />} />
