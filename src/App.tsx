c85bd916325b7ce290bb368ecdfada315d33a5f0
Geardung <Geardung@ya.ru>
2026-03-08 03:07:09 +0300

    Пофиксил редиректы

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import DevPage from './pages/DevPage'
 import AnalyticsPage from './pages/AnalyticsPage'
 
+function RedirectToBot() {
+  window.location.replace('https://t.me/Show_Circus_bot')
+  return null
+}
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/dev" replace />} />
+      <Route path="/" element={<RedirectToBot />} />
       <Route path="/dev" element={<DevPage />} />
       <Route path="/analytics" element={<AnalyticsPage />} />
-      <Route path="*" element={<Navigate to="/dev" replace />} />
+      <Route path="/analytics/*" element={<AnalyticsPage />} />
+      <Route path="*" element={<Navigate to="/analytics" replace />} />
     </Routes>
   )
 }
